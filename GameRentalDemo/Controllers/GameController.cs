6f213cf521629d22bdd19c8ff7fb2f7c9cd111d3
Simon van der Weele <vanderweelesimon@gmail.com>
2019-09-21 14:12:34 +0200

    initial commit

---
+++ GameRentalDemo/Controllers/GameController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameRentalDemo.Data;
+using GameRentalDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameRentalDemo.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public GameController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+
+            var games = applicationDbContext.GameModel;
+
+            return View(games);
+
+        }
+
+
+        public IActionResult ListGames()
+        {
+            var games = new List<GameModel>
+            {
+                new GameModel
+                {
+                    Genre = Genre.Action,
+                    Name = "Viewtiful Joe",
+                    InStockCount = 5,
+                },
+                new GameModel
+                {
+                    Genre = Genre.Action | Genre.SurvivalHorror,
+                    Name = "Bioshock",
+                    InStockCount = 5,
+                }
+            };
+
+            return View(games);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+            return View(applicationDbContext.GameModel.Find(id));
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            var entity = applicationDbContext.GameModel.Find(id); ;
+            if (entity != null)
+            {
+                applicationDbContext.GameModel.Remove(entity);
+                applicationDbContext.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Save(GameModel model)
+        {
+            CreateOrUpdateModel(model);
+            return RedirectToAction("Index");
+        }
+
+        private void CreateOrUpdateModel(GameModel model)
+        {
+            var dbModel = applicationDbContext.Find<GameModel>(model.Id);
+            if (dbModel != null && dbModel.Id != 0)
+            {
+                dbModel.Genre = model.Genre;
+                dbModel.Name = model.Name;
+                dbModel.InStockCount = model.InStockCount;
+            }
+            else
+            {
+                applicationDbContext.Add(model);
+            }
+
+            applicationDbContext.SaveChanges();
+        }
+    }
+}
---
+++ GameRentalDemo/Models/GameModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameRentalDemo.Models
+{
+    public class GameModel
+    {
+        [KeyAttribute]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public Genre Genre { get; set; }
+
+        public int InStockCount { get; set; }
+
+    }
+    public enum Genre
+    {
+        [Display(Name ="Action Genre")]
+        Action = 0,
+        [Display(Name = "Survival Horror Genre")]
+        SurvivalHorror = 1,
+        [Display(Name = "Platformer Genre")]
+        Platformer = 2,
+        [Display(Name = "Puzzle Genre")]
+        Puzzle = 3,
+    }
+}
